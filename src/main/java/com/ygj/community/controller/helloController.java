65185c5940906e825f3e861940d7df049d6889a2
十一 <563194726@qq.com>
2020-03-18 19:53:43 +0800

    frist commint

---
+++ src/main/java/com/ygj/community/controller/helloController.java
@@ -0,0 +1,18 @@
+package com.ygj.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author 十一
+ * @date 2020-03-17 19:58
+ */
+@Controller
+public class helloController {
+    @GetMapping("/index")
+    public String hello(){
+        return "index";
+    }
+}
---
+++ src/test/java/com/ygj/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygj.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
